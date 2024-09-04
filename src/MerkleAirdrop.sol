96ebf3d53c03f488972a5cc0a7bf947793ef44c0
Bahubali2550 <hs49271.rkt@gmail.com>
2024-09-05 03:18:01 +0530

    added DeployMerkleAirdrop

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {console} from "forge-std/Script.sol";
+
+contract MerkleAirdrop is EIP712 {
+    using SafeERC20 for IERC20;
+
+   // some list of addresses
+   // allow someone int he list to claim token
+   // for anyone to claim token we have to store their address to see if they are valid
+   // like if an artist want to distribute his free nft to only the owner of his premium nfts
+   // so if we store it in an array and everytime someone claims the token 
+   // we run a for loop so check if the msg.sender address is in array or not is really gas inefficient
+   // so here we use some kind of hash function called merkle which has a tree like structure and hash proof
+   // to validate the user  
+
+   error MerkleAirdrop__InvalidProof();
+   error MerkleAirdrop__AlreadyClaimed();
+   error MerkleAirdrop__InvalidSignature();
+
+   address[] claimers;
+   bytes32 private immutable i_merkleRoot;
+   IERC20 private immutable i_airdropToken;
+   mapping (address claimer => bool claimed) private s_hasClaimed;
+
+   bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account, uint256 amount)");
+
+   struct AirdropClaim {
+    address account;
+    uint256 amount;
+   }
+
+   event Claim(address account, uint256 amount);
+
+   constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("MerkleAirdrop","1") {
+    i_merkleRoot = merkleRoot;
+    i_airdropToken = airdropToken;
+   }
+
+   function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s) external {
+    // we are using signature, so that if anyone else want to claim token into my account but they pay the gas fee, they can do that
+    // they can call the claim function for me, i give my signature and wholla!! i get the token into my account without paying the gas fee
+    // such signed transaction is done through EIP-191 -> EIP-712
+    // like we do a transaction in wallet and metamsk asks for a sig.
+    if(s_hasClaimed[account])
+    {
+        revert MerkleAirdrop__AlreadyClaimed();
+    }
+    // check the signature
+    if(!_isValidSignature(account,getMessageHash(account,amount),v,r,s))
+    {
+        revert MerkleAirdrop__InvalidSignature();
+    }
+    // calculate using the account and the amount, the hash -> leaf node
+    bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+    if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
+        revert MerkleAirdrop__InvalidProof();
+    }
+    // prevent users from claiming multiple times
+    s_hasClaimed[account] = true;
+    emit Claim(account, amount);
+    // transfer the token
+    i_airdropToken.safeTransfer(account, amount);
+   }
+
+   function getMerkleRoot() external view returns (bytes32) {
+    return i_merkleRoot;
+   }
+
+   function getAirdropToken() external view returns (IERC20) {
+    return i_airdropToken;
+   }
+
+   function getMessageHash(address account, uint256 amount) public view returns(bytes32) {
+    return _hashTypedDataV4(
+        keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount})))
+    );
+   }
+
+   function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal pure returns(bool) {
+    (address actualSigner, , ) = ECDSA.tryRecover(digest, v, r, s);
+    return actualSigner == account;
+   }
+}
